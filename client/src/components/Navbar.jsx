857f4e2c134fb4ed92951e9bb00adf7e355a19e6
Atharva123987 <atharva.amberkar@gmail.com>
2023-06-05 20:33:47 +0530

    Minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                         <Link to={'/about'} class="mr-5 hover:text-gray-900">About</Link>
                         <Link to={'/contact'} class="mr-5 hover:text-gray-900">Contact</Link>
                     </nav>
-                    <Link to={"/login"}>
+                    <Link to={"/dashboard"}>
                     <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
